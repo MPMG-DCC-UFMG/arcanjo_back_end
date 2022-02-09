a09b187641c42e135bca793389a499e5b4637b6c
Rodrigo Denúbila <rdenubila@gmail.com>
2022-02-09 01:44:37 -0300

    use cli command if env var is set

--- src/services/cli.service.ts
+++ src/services/cli.service.ts
@@ -1,24 +1,30 @@
 import { exec } from 'child_process';
 import { AnalysisService } from './analysis.service';
 
+const resultsDir = process.env.RESULTS_DIR || "/m08/results";
+const cliDir = process.env.CLI_DIR;
+
 export class CliService {
 
     DOCKER_COMMAND: string[] = ["docker", "run", "--rm", "-v", "\"{PATH}:/m08/storage\"", "-v", __dirname + "../../results:/m08/results", "arcanjo-cli", "bash", "-c", "\"python3 run.py -p /m08/storage -i {ID} -t imagens -o /m08/results\""];
+    CLI_COMMAND: string[] = ["python3", `${cliDir}run.py`, "-p", "{PATH}", "-i", "{ID}", "-t", "imagens", "-o", resultsDir];
 
     constructor(
         private analysisService = new AnalysisService()
     ) { }
+
+    private getCommand(): string[] {
+        return cliDir ? this.CLI_COMMAND : this.DOCKER_COMMAND;
+    }
 
     async processAnalysis(id: number | string) {
         const dirPrefix = process.env.DIR_PREFIX || "";
         const analysis = await this.analysisService.getById(id);
 
         if (analysis && analysis.status === "created") {
-            console.log(__dirname);
             const finalPath: string = (dirPrefix + analysis.path).replace("//", "/");
-            console.log(finalPath);
 
-            const command: string[] = this.DOCKER_COMMAND.map(c => c
+            const command: string[] = this.getCommand().map(c => c
                 .replace("{PATH}", finalPath)
                 .replace("{ID}", `ID_${analysis.id}`)
             );
@@ -37,7 +43,7 @@ export class CliService {
     }
 
     runCommand(command: string[], updateLog?: (log: string) => void, finishCallback?: () => void) {
-        // const execution = spawn(command[0], command.slice(1));
+        console.log(`Running command: ${command.join(" ")}`);
         const execution = exec(command.join(" "));
 
         execution.stdout?.on("data", data => {
